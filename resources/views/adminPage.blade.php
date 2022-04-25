134d915a2712a9fb84d1ac7f17ac2a1fe1ba1d13
YesPro34 <echamkh786@gamil.com>
2022-04-25 14:34:44 +0000

    yassine v3

--- resources/views/adminPage.blade.php
+++ resources/views/adminPage.blade.php
@@ -3,25 +3,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
     <title>Document</title>
 </head>
 <body>
 
     <form>
-        <div class="mb-3">
-            <label  class="form-label">Name</label>
-            <input type="text" class="form-control" aria-describedby="name...">
+        <div class=" mb-3">
+            <label  >Name</label>
+            <input type="text" class="form-control" >
           </div>
         <div class="mb-3">
           <label class="form-label">Email address</label>
-          <input type="email" class="form-control"  aria-describedby="email..." >
+          <input type="email" class="form-control"   >
         </div>
         <div class="mb-3">
           <label class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
+          <input type="password" class="form-control" >
         </div>
         <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <input type="checkbox" class="form-check-input" >
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
